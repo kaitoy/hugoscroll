9de86024f7e3e3271289edb18829597e132b405f
kaitoy <kaitoy@pcap4j.org>
2016-07-02 22:39:18 -0600

    Remove post after.

--- static/js/index.js
+++ static/js/index.js
@@ -5,7 +5,6 @@ var $post = $('.post'),
 	$first = $('.post.first'),
 	$last = $('.post.last'),
 	$postholder = $('.post-holder'),
-	$postafter = $('.post-after'),
 	$sitehead = $('#site-head');
 
 /*globals jQuery, document */
@@ -17,18 +16,12 @@ var $post = $('.post'),
 		}, 1000);
     }
     $(document).ready(function(){
-        $postafter.each(function (e) {
-        	var bg = $(this).parent().css('background-color')
-        	$(this).css('border-top-color', bg)
-        })
-
         $('.post-title').each(function () {
         	var t = $(this).text(),
         	    index = $(this).parents('.post-holder').index();
         	$(this).parents('article').attr('id',t.toLowerCase().split(' ').join('-'));
         })
 
-        $('.post.last').next('.post-after').hide();
         $('ul li').before('<span class="bult fa fa-asterisk icon-asterisk"></span>')
         $('blockquote p').prepend('<span class="quo icon-quote-left"></span>')
                 .append('<span class="quo icon-quote-right"></span>')
