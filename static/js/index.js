b578f9b09aa0d5acd36df36a4aa7c00cb901f6f3
kaitoy <kaitoy@pcap4j.org>
2016-07-02 22:12:40 -0600

    Remove fixed navigator.

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,6 @@
 var $post = $('.post'),
 	$first = $('.post.first'),
 	$last = $('.post.last'),
-	$fnav = $('.fixed-nav'),
 	$postholder = $('.post-holder'),
 	$postafter = $('.post-after'),
 	$sitehead = $('#site-head');
@@ -33,53 +32,10 @@ var $post = $('.post'),
         $('.post-title').each(function () {
         	var t = $(this).text(),
         	    index = $(this).parents('.post-holder').index();
-        	$fnav.append("<a class='fn-item' item_index='"+index+"'>"+t+"</a>")
         	$(this).parents('article').attr('id',t.toLowerCase().split(' ').join('-'));
-        	$('.fn-item').click(function () {
-        		var i = $(this).attr('item_index'),
-        			s = $(".post[item_index='"+i+"']")
-
-        		$('html, body').animate({
-					scrollTop: s.offset().top
-				}, 400);
-
-        	})
         })
 
         $('.post.last').next('.post-after').hide();
-        if($sitehead.length) {
-            $(window).scroll( function () {
-                var w = $(window).scrollTop(),
-                    g = $sitehead.offset().top,
-                    h = $sitehead.offset().top + $(this).height()-100;
-
-                if(w >= g && w<=h) {
-                    $('.fixed-nav').fadeOut('fast')
-                } else {
-                    if($(window).width()>500)
-                      $('.fixed-nav').fadeIn('fast')
-                }
-
-                $post.each(function () {
-                    var f = $(this).offset().top,
-                        b = $(this).offset().top + $(this).height(),
-                        t = $(this).parent('.post-holder').index(),
-                        i = $(".fn-item[item_index='"+t+"']"),
-                        a = $(this).parent('.post-holder').prev('.post-holder').find('.post-after');
-
-                     $(this).attr('item_index', t);
-
-                    if(w >= f && w<=b) {
-
-                        i.addClass('active');
-                        a.fadeOut('slow')
-                    } else {
-                        i.removeClass('active');
-                        a.fadeIn('slow')
-                    }
-                })
-            });
-        }
         $('ul li').before('<span class="bult fa fa-asterisk icon-asterisk"></span>')
         $('blockquote p').prepend('<span class="quo icon-quote-left"></span>')
                 .append('<span class="quo icon-quote-right"></span>')
